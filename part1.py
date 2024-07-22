a9cf06be2423d2a0dac30fdc7108fccb8d1fc4f1
dylancsu <dylan.dean@csuglobal.edu>
2024-07-21 20:05:51 -0600

    Do Assignment

---
+++ part1.py
@@ -0,0 +1,4 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print(f"The sum is {x+y}")
---
+++ part2.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print(f"The product is {x*y}")
+print(f"The divisor is {x/y}")
